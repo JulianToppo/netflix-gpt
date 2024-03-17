63afed6a0911ced838b41d8250727edc72e075ab
JulianToppo <juliantoppo95@gmail.com>
2024-03-17 11:45:56 +0530

    Removal of header and background for gptpage

--- src/components/Gpt/GptHomePage.js
+++ src/components/Gpt/GptHomePage.js
@@ -5,9 +5,8 @@ import SearchBar from './SearchBar'
 import MovieRecommendation from './MovieRecommendation'
 const GptHomePage = () => {
   return (
-    <div>
-      <Header />
-      <BackgroungGpt />
+    <div className='w-full absolute h-screen' style={{backgroundImage: "url('https://assets.nflxext.com/ffe/siteui/vlv3/563192ea-ac0e-4906-a865-ba9899ffafad/6b2842d1-2339-4f08-84f6-148e9fcbe01b/IN-en-20231218-popsignuptwoweeks-perspective_alpha_website_small.jpg')"}}>
+   
       <SearchBar />
       <MovieRecommendation />
     </div>
